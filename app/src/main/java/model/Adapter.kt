c2a930cbc6741493af4c67f93a9d4988eb2278d3
Selfmade Music <elenazaharcenko24940@gmail.com>
2023-03-01 22:40:15 +0300

    add recycler and adapter

---
+++ app/src/main/java/model/Adapter.kt
@@ -0,0 +1,27 @@
+package model
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.control.R
+import kotlinx.android.synthetic.main.one_plant.view.*
+
+class Adapter(private var plants: List<ListPlant>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.one_plant,parent,false))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        holder.itemView.title.text=plants[position].title
+        holder.itemView.room.text=plants[position].room
+        holder.itemView.price.text=plants[position].price.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return plants.size
+    }
+
+}
